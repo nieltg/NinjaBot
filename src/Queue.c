d2f60970caa339cfcd5cdc1246a567201a98d3a0
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-03-21 22:31:30 +0700

    Added queue, renamed sourcefile003 to actions.c

---
+++ src/Queue.c
@@ -0,0 +1,50 @@
+#ifndef QUEUE
+#define QUEUE
+
+#define MAX_QUEUE_SIZE 100
+
+typedef struct {
+	int data[MAX_QUEUE_SIZE];
+	int head;
+	int tail;
+} Queue;
+
+void Queue_init(Queue *q) {
+	q->head = 0;
+	q->tail = 0;
+}
+
+int Queue_size(Queue *q) {
+	int size = q->tail - q->head;
+	if (size < 0) {
+		size += MAX_QUEUE_SIZE;
+	}
+	return size;
+}
+
+bool Queue_isFull(Queue *q) {
+	return Queue_size(q) >= MAX_QUEUE_SIZE-1;
+}
+
+bool Queue_isEmpty(Queue *q) {
+	return q->head == q->tail;
+}
+
+void Queue_push(Queue *q, int value) {
+	if (!Queue_isFull(q)) {
+		q->data[q->tail] = value;
+		q->tail = (q->tail + 1) % MAX_QUEUE_SIZE;
+	}
+}
+
+int Queue_pop(Queue *q) {
+	if (!Queue_isEmpty(q)) {
+		int res = q->data[q->head];
+		q->head = (q->head + 1) % MAX_QUEUE_SIZE;
+		return res;
+	} else {
+		return 0;
+	}
+}
+
+#endif
