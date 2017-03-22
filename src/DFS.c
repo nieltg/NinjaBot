6282f85d880715e8c31d6548afd7f62d294e392d
Daniel <nieltg@users.noreply.github.com>
2017-03-22 14:45:02 +0700

    Add more explanation on DFS

--- src/DFS.c
+++ src/DFS.c
@@ -2,6 +2,12 @@
 
 #include "common/Actions.c"
 
+/**
+ * @brief Explore the map using DFS technique.
+ *
+ * I.S.: Robot is about to enter a junction.
+ * F.S.: Robot is about to leave a junction.
+ */
 void DFS_do (bool _first)
 {
 	// Enter junction.
@@ -17,11 +23,17 @@ void DFS_do (bool _first)
 
 	for (int i = 0; i < cnt; i++)
 	{
+		// Workaround: Stop at 0 if there's only a path.
+
 		Actions_Junction_stopAt ((cnt > 1) ? 1 : 0);
+
+		// Move robot to junction and redo DFS.
 
 		Actions_Path_walk ();
 		DFS_do (false);
 		Actions_Path_walk ();
+
+		// Prepare for next junction operation.
 
 		Actions_Junction_begin ();
 	}
